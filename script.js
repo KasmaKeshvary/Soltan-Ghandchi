7416d749dcb5c77e9ba39a15829d5a211a4ba2e0
kasma <kasmakeshvary@gmail.com>
2023-03-27 14:20:16 +0430

    works by lightbox model

--- script.js
+++ script.js
@@ -30,9 +30,9 @@ var sampleJs = [
     Photo1: "/Photo/card21.png",
     Photo2: "/Photo/card22.png",
     Photo3: "/Photo/card23.png",
-    // Photo4: "/Photo/card24.png",
-    // Photo5: "/Photo/card25.png",
-    // Photo6: "/Photo/card26.png",
+    Photo4: "/Photo/card24.png",
+    Photo5: "/Photo/card25.png",
+    Photo6: "/Photo/card26.png",
   },
   {
     Name: "Gallery2",
@@ -40,8 +40,8 @@ var sampleJs = [
     Photo2: "/Photo/card28.png",
     Photo3: "/Photo/card31.png",
     Photo4: "/Photo/card32.png",
-    // Photo5: "/Photo/card33.png",
-    // Photo6: "/Photo/card25.png",
+    Photo5: "/Photo/card33.png",
+    Photo6: "/Photo/card25.png",
   },
   {
     Name: "Gallery3",
@@ -50,7 +50,7 @@ var sampleJs = [
     Photo3: "/Photo/card41.png",
     Photo4: "/Photo/card42.png",
     Photo5: "/Photo/card43.png",
-    // Photo6: "/Photo/card44.png",
+    Photo6: "/Photo/card44.png",
   },
   {
     Name: "Gallery4",
@@ -63,177 +63,114 @@ var sampleJs = [
   },
 ];
 
-var numOfslideOfgallery = [];
-var countphoto = {};
-var one = 0;
-
-dropsubmenu[1].onclick = showworks(sampleJs);
-
-function showworks(arr) {
+function showstore(arr) {
   var st = `<div>`;
   for (let i = 0; i < arr.length; i++) {
-    countphoto = Object.keys(arr[i]);
-    numOfslideOfgallery[i] = countphoto.length - 1 + one;
-    st += `<div class="slideshow-container">`;
+    var countphoto = Object.keys(arr[i]);
+    st += `<div class="dropdown">
+                       <div class="name flex" id="dp-${i}">
+                           <img class="galleryImg" src="${
+                             arr[i][countphoto[1]]
+                           }" >
+                           <div class="galleryName">${arr[i].Name}</div>
+                       </div>
+                       <div class="dropdown-content">
+                          <span class="close cursor">×</span>
+                          <div class="changeImg">
+                            <div class="perviosImg">❮</div>`;
     for (let j = 1; j < countphoto.length; j++) {
-      st += `<div class="mySlides fade" >
-            <div class="numbertext">${j} / ${countphoto.length - 1}</div>
-            <img src="${arr[i][countphoto[j]]}"
-                 style="width:100%">
-            <div class="text">${arr[i].Name}</div>
-        </div>`;
+      st += `<div class="Imgs${i} hide">
+                               <div class="numbertext">${j} / ${
+        countphoto.length - 1
+      }</div>
+                               <img class="Photo" src="${
+                                 arr[i][countphoto[j]]
+                               }" >
+                               </div>`;
     }
-    st += `<a class="prev" id="p-${
-      countphoto.length - 1
-    }" onclick="plusSlides(-1)">❮</a>
-        <a class="next" id="n-${
-          countphoto.length - 1
-        }" onclick="plusSlides(1)">❯</a>
-    </div>
-    <br>
-    <div style="text-align:center">`;
-    for (let j = 1; j < countphoto.length; j++) {
-      st += `<span class="dot" onclick="currentSlide(${j})"></span>`;
-    }
-    st += `</div>`;
-    one = countphoto.length - 1 + one;
+    st += `<div class="nextImg">❯</div>
+                          </div>
+                        </div>
+                    </div>`;
   }
   st += `</div>`;
   document.getElementById("rooot").innerHTML = st;
-  console.log(numOfslideOfgallery);
+}
+showstore(sampleJs);
+
+let dropName = document.getElementsByClassName("name");
+let dropDownContent = document.getElementsByClassName("dropdown-content");
+let close = document.getElementsByClassName("close");
+var current = 0;
+var currentphoto = 1;
+
+for (let j = 0; j < dropDownContent.length; j++) {
+  dropName[j].addEventListener("click", dropdown);
+  close[j].addEventListener("click", closeModal);
 }
 
-// let dropName = document.getElementsByClassName("name");
-// let dropDownContent = document.getElementsByClassName("dropdown-content");
-// let arrowDown = document.getElementsByClassName("arrowDown");
-// var current = 0;
-// var currentphoto = 1;
+var imgsw = {};
+var imgswOld = {};
 
-// for (let j = 0; j < dropDownContent.length; j++) {
-//   dropName[j].addEventListener("click", dropdown);
-// }
+function dropdown(e) {
+  var dpID = e.currentTarget.id.split("-")[1];
+  let old = current;
+  imgswOld = document.getElementsByClassName("Imgs" + old);
+  imgswOld[currentphoto].classList.remove("show");
+  imgswOld[currentphoto].classList.add("hide");
+  current = dpID;
+  imgsw = document.getElementsByClassName("Imgs" + current);
+  if (currentphoto > 0) {
+    imgsw[currentphoto].classList.remove("show");
+    imgsw[currentphoto].classList.add("hide");
+    currentphoto = 0;
+  }
+  imgsw[0].classList.remove("hide");
+  imgsw[0].classList.add("show");
+  imgsw[0].classList.add("Imgs");
 
-// var imgsw = {};
-// var imgswOld = {};
+  if (current === old) {
+    dropDownContent[current].classList.toggle("show");
+  } else {
+    dropDownContent[old].classList.remove("show");
+    dropDownContent[current].classList.add("show");
+  }
+}
 
-// function dropdown(e) {
-//   var dpID = e.currentTarget.id.split("-")[1];
-//   let old = current;
-//   imgswOld = document.getElementsByClassName("Imgs" + old);
-//   imgswOld[currentphoto].classList.remove("show");
-//   imgswOld[currentphoto].classList.add("hide");
-//   current = dpID;
-//   imgsw = document.getElementsByClassName("Imgs" + current);
-//   if (currentphoto > 0) {
-//     imgsw[currentphoto].classList.remove("show");
-//     imgsw[currentphoto].classList.add("hide");
-//     currentphoto = 0;
-//   }
-//   imgsw[0].classList.remove("hide");
-//   imgsw[0].classList.add("show");
-//   imgsw[0].classList.add("Imgs");
+let pervios = document.getElementsByClassName("perviosImg");
+let next = document.getElementsByClassName("nextImg");
 
-//   if (current === old) {
-//     dropDownContent[current].classList.toggle("show");
-//     arrowDown[current].classList.toggle("arrowUp");
-//   } else {
-//     arrowDown[old].classList.remove("arrowUp");
-//     dropDownContent[old].classList.remove("show");
-//     dropDownContent[current].classList.add("show");
-//     arrowDown[current].classList.add("arrowUp");
-//   }
-// }
-
-// let pervios = document.getElementsByClassName("perviosImg");
-// let next = document.getElementsByClassName("nextImg");
-
-// for (let j = 0; j < pervios.length; j++) {
-//   pervios[j].addEventListener("click", previouspage);
-// }
-
-// for (let j = 0; j < next.length; j++) {
-//   next[j].addEventListener("click", nextpage);
-// }
-
-// function previouspage() {
-//   let oldphoto = currentphoto;
-//   currentphoto -= 1;
-//   currentphoto = currentphoto < 0 ? 0 : currentphoto;
-//   imgsw[oldphoto].classList.remove("show");
-//   imgsw[oldphoto].classList.add("hide");
-//   imgsw[currentphoto].classList.remove("hide");
-//   imgsw[currentphoto].classList.add("show");
-// }
-
-// function nextpage() {
-//   let oldphoto = currentphoto;
-//   currentphoto += 1;
-//   currentphoto =
-//     currentphoto > imgsw.length - 1 ? imgsw.length - 1 : currentphoto;
-//   imgsw[oldphoto].classList.remove("show");
-//   imgsw[oldphoto].classList.add("hide");
-//   imgsw[currentphoto].classList.remove("hide");
-//   imgsw[currentphoto].classList.add("show");
-//   imgsw[currentphoto].classList.add("Imgs");
-// }
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-let prev = document.getElementsByClassName("prev");
-let next = document.getElementsByClassName("next");
-
-for (let j = 0; j < prev.length; j++) {
-  prev[j].addEventListener("click", pr);
+for (let j = 0; j < pervios.length; j++) {
+  pervios[j].addEventListener("click", previouspage);
 }
 
 for (let j = 0; j < next.length; j++) {
-  next[j].addEventListener("click", nx);
+  next[j].addEventListener("click", nextpage);
 }
 
-let countgalleryslide = {};
-
-function pr(e) {
-  countgalleryslide = e.currentTarget.id.split("-")[1];
-  console.log(countgalleryslide);
+function previouspage() {
+  let oldphoto = currentphoto;
+  currentphoto -= 1;
+  currentphoto = currentphoto < 0 ? 0 : currentphoto;
+  imgsw[oldphoto].classList.remove("show");
+  imgsw[oldphoto].classList.add("hide");
+  imgsw[currentphoto].classList.remove("hide");
+  imgsw[currentphoto].classList.add("show");
 }
 
-function nx(e) {
-  countgalleryslide = e.currentTarget.id.split("-")[1];
-  console.log(countgalleryslide);
+function nextpage() {
+  let oldphoto = currentphoto;
+  currentphoto += 1;
+  currentphoto =
+    currentphoto > imgsw.length - 1 ? imgsw.length - 1 : currentphoto;
+  imgsw[oldphoto].classList.remove("show");
+  imgsw[oldphoto].classList.add("hide");
+  imgsw[currentphoto].classList.remove("hide");
+  imgsw[currentphoto].classList.add("show");
+  imgsw[currentphoto].classList.add("Imgs");
 }
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  console.log(n);
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (let i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  slides[0].style.display = "block";
-  for (let i = 0; i < numOfslideOfgallery.length-1; i++) {
-    let firstslide = numOfslideOfgallery[i];
-    console.log(firstslide);
-    slides[firstslide].style.display = "block";
-    dots[firstslide].className += " active";
-  }
+function closeModal() {
+  dropDownContent[current].classList.remove("show");
+  dropDownContent[current].classList.add("hide");
 }
